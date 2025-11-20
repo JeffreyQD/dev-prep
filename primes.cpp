c7ccde49a6b0027831360da4719237dc731104b1
Ulricman <ulricmen@gmail.com>
2025-11-20 20:23:35 +0800

    write a more concise version

--- primes.cpp
+++ primes.cpp
@@ -4,43 +4,49 @@
 #include <variant>
 
 // * Returns the sorted primes not greater than `N` at compile time.
-template <size_t N, size_t n = N>
-constexpr auto primes()
-    -> std::conditional_t<n == N, size_t, std::array<int, n>> {
-  int idx = 0;
+template <size_t N, bool count = false>
+constexpr auto primes() {
+  if constexpr (count) {
+    int cnt = 0;
+    bool mask[N + 1] = {false, false};
+    for (int i = 2; i <= N; ++i) {
+      mask[i] = true;
+    }
 
-  // Use std::monostate to represent nothing.
-  std::conditional_t<n == N, std::monostate, std::array<int, n>> data;
-
-  bool mask[N + 1] = {false, false};
-  for (int i = 2; i <= N; ++i) {
-    mask[i] = true;
-  }
-
-  for (int i = 2; i <= N; ++i) {
-    if (mask[i]) {
-      if constexpr (n == N) {
-        ++idx;
-      } else {
-        data[idx++] = i;
-      }
-
-      for (int j = i << 1; j <= N; j += i) {
-        mask[j] = false;
+    for (int i = 2; i <= N; ++i) {
+      if (mask[i]) {
+        ++cnt;
+        for (int j = i << 1; j <= N; j += i) {
+          mask[j] = false;
+        }
       }
     }
-  }
 
-  if constexpr (n == N) {
-    return idx;
+    return cnt;
   } else {
+    int idx = 0;
+    constexpr size_t n = primes<N, true>();
+    bool mask[N + 1] = {false, false};
+    for (int i = 2; i <= N; ++i) {
+      mask[i] = true;
+    }
+
+    std::array<int, n> data{};
+    for (int i = 2; i <= N; ++i) {
+      if (mask[i]) {
+        data[idx++] = i;
+        for (int j = i << 1; j <= N; j += i) {
+          mask[j] = false;
+        }
+      }
+    }
+
     return data;
   }
 }
 
 constexpr size_t N = 100000;
-constexpr size_t n = primes<N, N>();
-constexpr auto data = primes<N, n>();
+constexpr auto data = primes<N>();
 
 int main() {
   for (int x : data) {
